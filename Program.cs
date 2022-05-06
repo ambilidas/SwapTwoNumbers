c8f307b34b1ee72c858e1f312967750b8bd0b872
Ambili das v s <ambili.kan@gmail.com>
2022-05-06 22:53:04 +0530

    Swaping of numbers is done

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SwapTwoNumbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("a={0} b={1}",a,b);
+            int c = a;
+            a=b;
+            b=c;
+            Console.WriteLine("swaping done a={0} b={1}", a, b);
+        }
+    }
+}
